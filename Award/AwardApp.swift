71c41436c93c7ce63651dbdbd6e1323dc0beae18
Vadim Vanatov <79990006611@yandex.ru>
2023-10-31 15:06:30 +0300

    Initial Commit

---
+++ Award/AwardApp.swift
@@ -0,0 +1,17 @@
+//
+//  AwardApp.swift
+//  Award
+//
+//  Created by HOLY NADRUGANTIX on 31.10.2023.
+//
+
+import SwiftUI
+
+@main
+struct AwardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Award/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Award
+//
+//  Created by HOLY NADRUGANTIX on 31.10.2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
